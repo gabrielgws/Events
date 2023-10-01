01522e713e4c60c67bed83f39d6fa0ca94f9ac43
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-10-01 20:45:40 -0300

    Resgatando parâmetros de URL & navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
         {{-- CSS Bootstrap --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
@@ -22,6 +23,32 @@
         <script src="/js/scripts.js"></script>
     </head>
     <body>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="nav-brand">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            </a>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="/" class="nav-link">Criar eventos</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       @yield('content')
         
       <footer>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,5 +5,9 @@
   @section('content')
       
     <h1>Tela de produtos!</h1>
+
+    @if ($busca != '')
+      <p>O usuário está buscando por: {{ $busca }}</p>
+    @endif
   
   @endsection
